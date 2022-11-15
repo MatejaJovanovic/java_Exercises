d335f96704081d01d1b31f00f14a0f007ed10d0b
matejajovanovic <mateja.beba@gmail.com>
2022-11-15 15:50:01 +0100

    HashMap, custom input string and sample text

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import BalloonProblem.Solution;
 import BalloonProblem.Solution2;
+import BalloonProblem.Solution3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Solution2 s = new Solution2();
-        int num =0;
-        num = s.solution("BALLOOOOOasdksaldkaskldsaNNNNNBALLOON BALLOON BALLOON, BALLOON.,BALLOON,BALLOON,BALLOONBALLOONBALLBALLOONOON");
-        System.out.println("Number of iterations is:" + num);
+       //System.out.println("Hello world!");
+       //Solution2 s = new Solution2();
+       //int num =0;
+       //num = s.solution("BALLO");
+       //System.out.println("Number of iterations is:" + num);
+
+        Solution3 s = new Solution3();
+        int num;
+        num = s.solution();
+        System.out.println("Number of possible iterations is: " + num);
+
     }
 }
-//QAWABAWONL
+//BALLOOOOOasdksaldkaskldsaNNNNNBALLOON BALLOON BALLOON, BALLOON.,BALLOON,BALLOON,BALLOONBALLOONBALLBALLOONOON
